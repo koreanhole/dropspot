d5485968f39172b587a9b5949b55b37b2cc83214
koreanhole <koreanhole1@gmail.com>
2025-01-22 11:09:14 +0900

    change to cupertino app

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:dropspot/components/camera_button.dart';
 import 'package:dropspot/components/image_viewer.dart';
 import 'package:dropspot/components/recognized_text.dart';
 import 'package:dropspot/providers/parking_image_provider.dart';
-import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:logger/web.dart';
 import 'package:provider/provider.dart';
 
@@ -29,11 +29,11 @@ class DropspotApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return CupertinoApp(
+      title: 'Dropspot',
+      theme: CupertinoThemeData(
+        primaryColor: CupertinoColors.systemBlue,
+        brightness: Brightness.light,
       ),
       home: const HomeScreen(),
     );
@@ -45,12 +45,8 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text("Dropspot"),
-      ),
-      body: Center(
+    return CupertinoPageScaffold(
+      child: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[

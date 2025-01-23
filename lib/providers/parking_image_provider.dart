d12772b441d8d2a0b13ec8e85774351a4097dd44
koreanhole <koreanhole1@gmail.com>
2025-01-24 03:26:52 +0900

    사진찍은 후 업데이트 되지 않던 문제 수정

--- lib/providers/parking_image_provider.dart
+++ lib/providers/parking_image_provider.dart
@@ -14,13 +14,7 @@ class ParkingImageProvider with ChangeNotifier {
   File? _image;
 
   ParkingImageProvider() {
-    _getParkingImage().then((File parkingImage) {
-      if (parkingImage.existsSync()) {
-        _image = parkingImage;
-        notifyListeners();
-        Logger().d('ParkingImage loaded');
-      }
-    });
+    _loadLatestParkingImage();
   }
 
   Future<void> saveImageToFiles({required XFile image}) async {
@@ -32,10 +26,35 @@ class ParkingImageProvider with ChangeNotifier {
     Logger().d('ParkingImage saved');
   }
 
+  Future<void> _loadLatestParkingImage() async {
+    final Directory directory = await getApplicationDocumentsDirectory();
+    final List<FileSystemEntity> files = directory.listSync()
+      ..sort((a, b) {
+        // 수정된 시간 기준으로 정렬 (최신 파일이 맨 앞으로)
+        return b.statSync().modified.compareTo(a.statSync().modified);
+      });
+
+    // 가장 최신 파일 로드
+    final latestFile = files.isNotEmpty ? files.first : null;
+    if (latestFile != null && latestFile is File) {
+      _image = latestFile;
+      notifyListeners();
+      Logger().d('Latest ParkingImage loaded: ${_image!.path}');
+    }
+
+    // 최신 파일 제외 나머지 삭제
+    for (int i = 1; i < files.length; i++) {
+      if (files[i] is File) {
+        await files[i].delete(); // 파일 삭제
+      }
+    }
+  }
+
   Future<File> _getParkingImage() async {
     final Directory directory = await getApplicationDocumentsDirectory();
     final String path = directory.path;
-    final File parkingImage = File('$path/$imageFileName');
+    final File parkingImage =
+        File('$path/$imageFileName${DateTime.now().microsecondsSinceEpoch}');
     return parkingImage;
   }
 }

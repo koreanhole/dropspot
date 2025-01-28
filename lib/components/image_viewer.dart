ec9500a93e944fffa8f819a60d2f10a0adb38cbe
koreanhole <koreanhole1@gmail.com>
2025-01-28 15:55:23 +0900

    Add zoom for image

--- lib/components/image_viewer.dart
+++ lib/components/image_viewer.dart
@@ -1,19 +1,40 @@
+import 'dart:async';
+
 import 'package:dropspot/components/camera_aspect_ratio_preset.dart';
 import 'package:dropspot/providers/parking_image_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+
+const double minImageScale = 1.0;
+const double maxImageScale = 10.0;
+const Duration resetImageScaleDuration = Duration(seconds: 2);
 
 class ImageViewer extends StatelessWidget {
   const ImageViewer({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final TransformationController transformationController =
+        TransformationController(Matrix4.identity());
+
     final parkingImagePath = context.watch<ParkingImageProvider>().imagePath;
     return CameraAspectRatioPreset(
-      child: Image.asset(
-        parkingImagePath,
-        fit: BoxFit.cover,
-        key: ValueKey(DateTime.now().millisecondsSinceEpoch),
+      child: InteractiveViewer(
+        minScale: minImageScale,
+        maxScale: maxImageScale,
+        key: ValueKey(
+          DateTime.now().millisecondsSinceEpoch,
+        ),
+        transformationController: transformationController,
+        onInteractionEnd: (details) {
+          Timer(resetImageScaleDuration, () {
+            transformationController.value = Matrix4.identity();
+          });
+        },
+        child: Image.asset(
+          parkingImagePath,
+          fit: BoxFit.cover,
+        ),
       ),
     );
   }

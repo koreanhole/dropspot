ee131909cad3a7d9d72f6e32ee4dc419b169f29a
Soonhyeong Kwon <koreanhole1@gmail.com>
2026-04-21 19:30:30 +0900

    feat: remove activity transition api and notification functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 
 import 'package:app_links/app_links.dart';
-import 'package:dropspot/base/activity_recognition_handler.dart';
 import 'package:dropspot/base/drop_spot_router.dart';
 import 'package:dropspot/base/flutter_channel/flutter_channel_helper.dart';
 import 'package:dropspot/base/theme/colors.dart';
@@ -40,19 +39,12 @@ class _DropspotAppState extends State<DropspotApp> {
   void initState() {
     super.initState();
     initializeApplink();
-    initializeActivityRecognition();
   }
 
   @override
   void dispose() {
     terminateApplink();
     super.dispose();
-  }
-
-  Future<void> initializeActivityRecognition() async {
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      ActivityRecognitionHandler().init();
-    });
   }
 
   Future<void> initializeApplink() async {
